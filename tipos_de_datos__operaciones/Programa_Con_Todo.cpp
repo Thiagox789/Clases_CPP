2a52caa5c954720974fe3f1fdc596a337f0f0689
Thiagox <almadathiagoignacio@gmail.com>
2024-10-27 13:35:23 -0300

    Bien

--- tipos_de_datos__operaciones/Programa_Con_Todo.cpp
+++ tipos_de_datos__operaciones/Programa_Con_Todo.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 using namespace std;
-int variableglobal=11;
-int main()
-{
-    int variable1=10;
+
+int variableglobal = 11;
+
+int main() {
+    int variable1 = 10;
     cout << "La variable local es: " << variable1 << endl;
-	cout << "La variable global es: " << variableglobal << endl;
-	int num1=10;
-	int num2=11;
-	int sum=num1+num2;
-	
-	int prod=num1*num2;
-	
-	float divicion=num1/num2;
-	
-	float resto=num1 % num2;
-	
-	cout << "La suma de num1 + num2 es: " << sum << endl;
-	cout << "el producto de num1 * num2 es: " << prod << endl;
-	cout << "el cociente de num1 / num2 es: " << divicion << endl;
-	cout << "el resto de num1 y num2 es: " << resto << endl;
+    cout << "La variable global es: " << variableglobal << endl;
+
+    int num1 = 10;
+    int num2 = 11;
+    int sum = num1 + num2;
+    int prod = num1 * num2;
+    float division = static_cast<float>(num1) / num2;  // Para obtener un resultado flotante
+    int resto = num1 % num2;
+
+    cout << "La suma de num1 + num2 es: " << sum << endl;
+    cout << "El producto de num1 * num2 es: " << prod << endl;
+    cout << "El cociente de num1 / num2 es: " << division << endl;
+    cout << "El resto de num1 y num2 es: " << resto << endl;
     return 0;
 }
