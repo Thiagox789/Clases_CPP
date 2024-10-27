3b9438b54a353e5a4469e67ae9ba7b5181e48780
Thiagox <almadathiagoignacio@gmail.com>
2024-10-27 20:47:40 -0300

    Ejercico 1 de condiciones terminado

---
+++ Condicionales/Ejercicio1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+//Escribir un programa que pregunte al usuario su edad y muestre por 
+//pantalla si es mayor de edad o no.
+
+int main()
+{
+    int edad;
+    cout<<"Ingrese su edad por favor:";
+    cin>>edad;
+    if (edad>=18)
+    {
+        cout<<"Usted es mayor de edad";
+    }
+    else
+    {
+        cout<<"Usted es menor de edad";
+    }
+
+
+
+
+
+    return 0;
+}
