0ec3d859eb31fa089395384ddc95a5261a9180d5
Thiagox <almadathiagoignacio@gmail.com>
2024-10-27 21:37:39 -0300

    Ejercicio 5

--- Condicionales/Ejercicio5.cpp
+++ Condicionales/Ejercicio5.cpp
@@ -13,7 +13,30 @@ int main()
     cin >> numero2;
     cout << "Ingresa el numero 3: ";
     cin >> numero3;
-    
-    
+    // Comparar los números y mostrarlos en orden
+    if (numero1 <= numero2 && numero1 <= numero3) {
+        cout << numero1 << " ";
+        if (numero2 <= numero3) {
+            cout << numero2 << " " << numero3;
+        } else {
+            cout << numero3 << " " << numero2;
+        }
+    } else if (numero2 <= numero1 && numero2 <= numero3) {
+        cout << numero2 << " ";
+        if (numero1 <= numero3) {
+            cout << numero1 << " " << numero3;
+        } else {
+            cout << numero3 << " " << numero1;
+        }
+    } else {
+        cout << numero3 << " ";
+        if (numero1 <= numero2) {
+            cout << numero1 << " " << numero2;
+        } else {
+            cout << numero2 << " " << numero1;
+        }
+    }
+
+    cout << endl; 
     return 0;
 }
